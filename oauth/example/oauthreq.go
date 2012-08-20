dda5559cabeeb7d872f708632825b7cd1b89f825
Anthony Starks <ajstarks@gmail.com>
2012-08-20 12:03:47 +1000

    goauth2: add -auth -token -redirect, and -ap flags allowing the example to work with other OAuth2 providers Tested with Microsoft, Google, and Meetup
    R=adg
    CC=golang-dev
    http://codereview.appspot.com/6459103
    
    Committer: Andrew Gerrand <adg@golang.org>

--- oauth/example/oauthreq.go
+++ oauth/example/oauthreq.go
@@ -19,10 +19,14 @@ import (
 var (
 	clientId     = flag.String("id", "", "Client ID")
 	clientSecret = flag.String("secret", "", "Client Secret")
+	authURL      = flag.String("auth", "https://accounts.google.com/o/oauth2/auth", "Authorization URL")
+	tokenURL     = flag.String("token", "https://accounts.google.com/o/oauth2/token", "Token URL")
 	apiURL       = flag.String("api", "https://www.googleapis.com/auth/userinfo.profile", "API URL")
+	redirectURL  = flag.String("redirect", "http://localhost/", "Redirect URL")
 	apiRequest   = flag.String("req", "https://www.googleapis.com/oauth2/v1/userinfo", "API request")
 	code         = flag.String("code", "", "Authorization Code")
 	cachefile    = flag.String("cachefile", "request.token", "Token cache file")
+	authparam    = flag.String("ap", "", "Authorization parameter")
 	cache        = flag.Bool("cache", false, "Read token from cache")
 
 	tokenCache oauth.CacheFile
@@ -44,22 +48,21 @@ func main() {
 		fmt.Fprint(os.Stderr, usageMsg)
 		return
 	}
-
 	// Set up a configuration
 	config := &oauth.Config{
 		ClientId:     *clientId,
 		ClientSecret: *clientSecret,
 		Scope:        *apiURL,
-		AuthURL:      "https://accounts.google.com/o/oauth2/auth",
-		TokenURL:     "https://accounts.google.com/o/oauth2/token",
-		RedirectURL:  "http://localhost/",
+		AuthURL:      *authURL,
+		TokenURL:     *tokenURL,
+		RedirectURL:  *redirectURL,
 	}
 
 	// Step one, get an authorization code from the data provider.
 	// ("Please ask the user if I can access this resource.")
 	if *code == "" && !*cache {
 		url := config.AuthCodeURL("")
-		fmt.Println("Visit this URL to get a code, then run again with -code=YOUR_CODE")
+		fmt.Println("Visit this URL to get a code, then run again with -code=YOUR_CODE\n")
 		fmt.Println(url)
 		return
 	}
@@ -74,11 +77,11 @@ func main() {
 	if !*cache {
 		tok, err := t.Exchange(*code)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Exchange:", err)
 		}
 		err = tokenCache.PutToken(tok)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Cache write:", err)
 		}
 		fmt.Printf("Token is cached in %v\n", tokenCache)
 		return
@@ -91,15 +94,19 @@ func main() {
 		// ("Here's the token, let me in!")
 		ctoken, err := tokenCache.Token()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Cache read:", err)
 		}
 		t.Token = &oauth.Token{AccessToken: ctoken.AccessToken}
+		// Tack on the extra parameters, if specified.
+		if *authparam != "" {
+			*apiRequest += *authparam + ctoken.AccessToken
+		}
 	}
 
 	// Make the request.
 	r, err := t.Client().Get(*apiRequest)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Request:", err)
 	}
 	defer r.Body.Close()
 	// Write the response to standard output.
