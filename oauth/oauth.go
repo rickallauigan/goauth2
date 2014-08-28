cb9484d494416c5f78dfe6ea9a847d54ef1f07b0
Andrew Gerrand <adg@golang.org>
2014-08-28 15:27:49 +1000

    goauth2: add more sites to the list of badly behaved OAuth2 providers
    Fixes issue 39.
    Fixes issue 40.
    
    LGTM=bradfitz
    R=bradfitz
    CC=golang-codereviews
    https://codereview.appspot.com/139740043

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -386,8 +386,10 @@ func (t *Transport) AuthenticateClient() error {
 // - Google only accepts URL param (not spec compliant?), not Auth header
 func providerAuthHeaderWorks(tokenURL string) bool {
 	if strings.HasPrefix(tokenURL, "https://accounts.google.com/") ||
-		strings.HasPrefix(tokenURL, "https://github.com/") {
-		// Google and GitHub fail to implement the OAuth2 spec fully?
+		strings.HasPrefix(tokenURL, "https://github.com/") ||
+		strings.HasPrefix(tokenURL, "https://api.instagram.com/") ||
+		strings.HasPrefix(tokenURL, "https://www.douban.com/") {
+		// Some sites fail to implement the OAuth2 spec fully.
 		return false
 	}
 
